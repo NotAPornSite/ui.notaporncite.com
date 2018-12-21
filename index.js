72b363f358824a0245ee92000648f1ec3b08271f
NotKeithRichards <notkeithrichards1@gmail.com>
2018-12-20 21:14:04 -0600

    Add infinite scroll poc

--- index.js
+++ index.js
@@ -8,10 +8,49 @@
 'use strict';
 require('dotenv').config()
 
+/* App dependencies
+ */
+
 const express = require('express'),
       app     = express();
+// {
+const data    = require('./modules/items.js');
+
+const pageCount = 15;
+
+/* END
+ * ---
+ *
+ * Auxiliary functions
+ */
+
+const getPage = page => {
+    let start = page*pageCount;
+    if (start > data.length) {
+        return [];
+    }
+    if (start + pageCount > data.length) {
+        return data.slice(start);
+    }
+
+    return data.slice(page*pageCount, (page*pageCount)+pageCount);
+}
+
+/* END
+ * ---
+ *
+ * App config
+ */
 
 app.use('/', express.static('./public'));
+
+app.get('/items', (req, res) => {
+    const { page } = req.query;
+    if (!page) {
+        return res.json(getPage(0));
+    }
+    res.json(getPage(page));
+});
 
 // LETS GO
 app.listen(process.env.PORT, () => console.log(`listening on port ${process.env.PORT}!`) )
--- public/js/main.js
+++ public/js/main.js
@@ -5,12 +5,26 @@ const npc = (function() {
 
     /* General util
      */
+    let pipe = function() {
+        return arg => {
+            Array.from(arguments).forEach(fn => fn(arg));
+            return arg;
+        }
+    }
 
     let on = (elem, evt, cb) => {
         elem.addEventListener(evt, cb);
     }
 
     let q = query => document.querySelector(query);
+
+    /* elements */
+
+    let div = () => document.createElement('div');
+
+    let img = () => document.createElement('img');
+
+    let addClass = clazz => { return elem => elem.classList.add(clazz) };
 
 
     /* Modal
@@ -34,5 +48,43 @@ const npc = (function() {
             console.error(`No element by id '${id}'!`);
     };
 
-    return { hide, show, on, q };
+
+    /* Gallery
+     */
+
+    let galleryItem = src => {
+        const construct = pipe(addClass('gallery-item')),
+              item      = construct(div());
+        item.appendChild(img()).src = src;
+
+        return item;
+    }
+
+    let getItems = page => {
+        return new Promise(function(resolve, reject) {
+            let xhr = new XMLHttpRequest();
+            xhr.addEventListener('load', function(e) {
+                if (this.status === 200) {
+                    resolve(JSON.parse(this.responseText));
+                } else {
+                    reject(this.status);
+                }
+            });
+            xhr.open('GET', `/items?page=${page}`);
+            xhr.send();
+        });
+    }
+
+    return {
+        hide,
+        show,
+        on,
+        q,
+        div,
+        img,
+        addClass,
+        pipe,
+        galleryItem,
+        getItems
+    };
 })();
