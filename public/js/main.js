bb9d667b80006e8f06362a4504a60916551d9b7f
NotKeithRichards <notkeithrichards1@gmail.com>
2018-12-22 20:43:32 -0600

    Begin implementing gallery.
    Add test page for test events

--- public/js/main.js
+++ public/js/main.js
@@ -26,6 +26,10 @@ const npc = (function() {
 
     let addClass = clazz => { return elem => elem.classList.add(clazz) };
 
+    let rmClass  = clazz => { return elem => elem.classList.remove(clazz) };
+
+    let addChild = child => { return elem => elem.appendChild(child) };
+
 
     /* Modal
      */
@@ -52,14 +56,6 @@ const npc = (function() {
     /* Gallery
      */
 
-    let galleryItem = src => {
-        const construct = pipe(addClass('gallery-item')),
-              item      = construct(div());
-        item.appendChild(img()).src = src;
-
-        return item;
-    }
-
     let getItems = page => {
         return new Promise(function(resolve, reject) {
             let xhr = new XMLHttpRequest();
@@ -75,6 +71,86 @@ const npc = (function() {
         });
     }
 
+    let GalleryItem = function(domNode)
+    {
+        this.domNode = domNode;
+
+        this._focus = addClass('is-focused');
+        this._blur = rmClass('is-focused');
+
+        on(domNode, 'touchstart', e => {
+            this._focus(domNode);
+        })
+        on(domNode, 'touchend', e => {
+            this._blur(domNode);
+        })
+    }
+
+    let Gallery = function(domNode) {
+        this._nearestHundred = num => Math.round(num/100) * 100;
+
+        this._breakpoints = {
+            '100,200': 0,
+            '200,300': 0,
+            '300,400': 0,
+            '400,500': 1,
+            '500,600': 1,
+            '600,700': 1,
+            '700,800': 2,
+            '800,900': 2,
+           '900,1000': 2
+       };
+
+       this._getBreakpoint = () => {
+           let upper = this._nearestHundred(window.innerWidth),
+               lower = this._nearestHundred(window.innerWidth) - 100;
+           return this._breakpoints[`${lower},${upper}`];
+        };
+
+        this._previous = this._getBreakpoint();
+
+        this._items = [];
+
+        this._reorder = () => {
+            // TODO implement me!
+            console.log('reorder gallery');
+        };
+
+        this.addItem = item => {
+            this._items.push(item);
+            this.domNode.appendChild(item.domNode);
+        }
+
+        this.domNode = domNode;
+
+        on(window, 'resize', e => {
+            let current = this._getBreakpoint();
+            if (current && current != this._previous) {
+                this._reorder();
+                this._previous = current;
+            }
+        });
+    };
+
+    let galleryItem = src => {
+        const construct = pipe(
+                            addClass('gallery-item'),
+                            addChild(
+                                pipe(
+                                    addClass('gi-img'),
+                                    addChild(img()),
+                                    addChild(pipe(addClass('gi-img-overlay'))(div()))
+                                )(div())
+                            ),
+                            addChild( pipe(addClass('gi-content'))(div()) )
+                        );
+
+        const item = construct(div());
+        item.querySelector('img').src = src;
+        return new GalleryItem(item);
+    }
+
+
     return {
         hide,
         show,
@@ -83,8 +159,10 @@ const npc = (function() {
         div,
         img,
         addClass,
+        rmClass,
         pipe,
         galleryItem,
-        getItems
+        getItems,
+        Gallery
     };
 })();
